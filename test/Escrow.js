ddb8278eebaeff8448d10b83a4d20b6b8c41b04a
Vageshwar <vageshwaryadav@gmail.com>
2023-05-14 22:23:07 +0530

    feat: Deployement logic added for localhost deployment via hardhat

--- test/Escrow.js
+++ test/Escrow.js
@@ -32,7 +32,7 @@ describe('Escrow', () => {
         await transaction.wait()
 
         // List Property
-        transaction = await escrow.connect(seller).list(1);
+        transaction = await escrow.connect(seller).list(1, buyer.address, tokens(10), tokens(4));
         await transaction.wait();
     })
 
@@ -63,6 +63,75 @@ describe('Escrow', () => {
         it('Updates as listed', async () => {
             const result = await escrow.isListed(1);
             expect(result).to.be.equal(true);
+        })
+    })
+
+    describe('Deposits', ()=> {
+        it('Updates contract balance', async ()=> {
+            const transaction = await escrow.connect(buyer).depositEarnest(1, {value: tokens(10)})
+            await transaction.wait()
+            const result = await escrow.getBalance();
+            expect(result).to.be.equal(tokens(10));
+        })
+    })
+
+    describe('Inspections', ()=> {
+        it('Updates inspectin status', async ()=> {
+            const transaction = await escrow.connect(inspector).updateInspectionStatus(1,true)
+            await transaction.wait()
+            const result = await escrow.inspectionPassed(1);
+            expect(result).to.be.equal(true);
+        })
+    })
+
+    describe('Approval', () => {
+        beforeEach(async () => {
+            let transaction = await escrow.connect(buyer).approveSale(1)
+            await transaction.wait()
+
+            transaction = await escrow.connect(seller).approveSale(1)
+            await transaction.wait()
+
+            transaction = await escrow.connect(lender).approveSale(1)
+            await transaction.wait()
+        })
+
+        it('Updates approval status', async () => {
+            expect(await escrow.approval(1, buyer.address)).to.be.equal(true)
+            expect(await escrow.approval(1, seller.address)).to.be.equal(true)
+            expect(await escrow.approval(1, lender.address)).to.be.equal(true)
+        })
+    })
+
+    describe('Sale', () => {
+        beforeEach(async () => {
+            let transaction = await escrow.connect(buyer).depositEarnest(1, { value: tokens(10) })
+            await transaction.wait()
+
+            transaction = await escrow.connect(inspector).updateInspectionStatus(1, true)
+            await transaction.wait()
+
+            transaction = await escrow.connect(buyer).approveSale(1)
+            await transaction.wait()
+
+            transaction = await escrow.connect(seller).approveSale(1)
+            await transaction.wait()
+
+            transaction = await escrow.connect(lender).approveSale(1)
+            await transaction.wait()
+
+            await lender.sendTransaction({ to: escrow.address, value: tokens(10) })
+
+            transaction = await escrow.connect(seller).finalizeSale(1)
+            await transaction.wait()
+        })
+
+        it('Updates ownership', async () => {
+            expect(await realEstate.ownerOf(1)).to.be.equal(buyer.address)
+        })
+
+        it('Updates balance', async () => {
+            expect(await escrow.getBalance()).to.be.equal(0)
         })
     })
 })
